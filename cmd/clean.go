f09c796c9347aa1144624a5021ec86fda0341d37
kakeng <kakengloh@gmail.com>
2022-08-18 17:01:23 +0800

    Add confirmation to `clean` command

--- cmd/clean.go
+++ cmd/clean.go
@@ -2,6 +2,7 @@ package cmd
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/kakengloh/tsk/driver"
 	"github.com/spf13/cobra"
@@ -10,11 +11,21 @@ import (
 func NewCleanCommand() *cobra.Command {
 	return &cobra.Command{
 		Use:   "clean",
-		Short: "Deletes all the data",
+		Short: "Clear all data",
 		RunE: func(cmd *cobra.Command, args []string) error {
+			var input string
+
+			cmd.Println("Are you sure you want to clear all data? This action cannot be undone (y/n)")
+			cmd.Print(">> ")
+			fmt.Scanf("%s", &input)
+
+			if strings.ToLower(input) != "y" {
+				return nil
+			}
+
 			err := driver.RemoveBolt()
 			if err == nil {
-				fmt.Println("Data deleted successfully")
+				fmt.Println("Data cleared successfully ✅")
 			}
 			return err
 		},
