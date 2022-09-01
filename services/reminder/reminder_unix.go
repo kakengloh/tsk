1dba44ccf1ee1ab6a7ed6af78407cb16727032a3
kakeng <kakengloh@gmail.com>
2022-09-01 19:35:57 +0800

    Add unix reminder

---
+++ services/reminder/reminder_unix.go
@@ -0,0 +1,14 @@
+//go:build linux && !darwin && !windows
+// +build linux,!darwin,!windows
+
+package reminder
+
+func Start() error {
+	// TODO
+	return nil
+}
+
+func Stop() error {
+	// TODO
+	return nil
+}
