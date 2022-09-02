6cb30423790c4859135c3b76d5fef6c242aa0a9b
kakeng <kakengloh@gmail.com>
2022-09-02 15:21:49 +0800

    Create VBScript file on windows

--- services/reminder/reminder_windows.go
+++ services/reminder/reminder_windows.go
@@ -7,26 +7,64 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
+	"path"
+
+	"github.com/kakengloh/tsk/driver"
 )
 
+func createVbsPayload(binPath string) string {
+	return fmt.Sprintf(`
+		Set WshShell = CreateObject("WScript.Shell")
+		WshShell.Run "%s notify", 0
+		Set WshShell = Nothing
+	`, binPath)
+}
+
 func Start() error {
+	// Find tsk path
 	binPath, err := os.Executable()
 	if err != nil {
 		return err
 	}
 
+	// Find tsk data dir
+	dataPath, err := driver.GetDataDir()
+	if err != nil {
+		return err
+	}
+
+	// Find wscript path
+	wscriptPath, err := exec.LookPath("wscript.exe")
+	if err != nil {
+		return fmt.Errorf("unable to find wscript: %w", err)
+	}
+
+	// Build VBScript path
+	vbScriptPath := path.Join(dataPath, "TskReminder.vbs")
+
 	// Check if scheduled task exists
 	err = exec.Command("schtasks", "/query", "/tn", "TskReminder").Run()
 	// Delete scheduled task if exists
 	if err == nil {
-		err = exec.Command("schtasks", "/delete", "/tn", "TskReminder", "/f").Run()
+		err = Stop()
 		if err != nil {
-			return fmt.Errorf("failed to delete scheduled task: %w", err)
+			return err
 		}
 	}
 
+	// Create VBScript file
+	f, err := os.OpenFile(vbScriptPath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		return fmt.Errorf("failed to open VBScript file: %w", err)
+	}
+	defer f.Close()
+	_, err = f.Write([]byte(createVbsPayload(binPath)))
+	if err != nil {
+		return fmt.Errorf("failed to create VBScript: %w", err)
+	}
+
 	// Create scheduled task
-	err = exec.Command("schtasks", "/create", "/sc", "MINUTE", "/mo", "1", "/tn", "TskReminder", "/tr", binPath+" notify").Run()
+	err = exec.Command("schtasks", "/create", "/np", "/sc", "MINUTE", "/mo", "1", "/tn", "TskReminder", "/tr", wscriptPath+" "+vbScriptPath).Run()
 	if err != nil {
 		return fmt.Errorf("failed to create scheduled task: %w", err)
 	}
@@ -41,10 +79,10 @@ func Start() error {
 }
 
 func Stop() error {
-	// End scheduled task
-	err := exec.Command("schtasks", "/end", "/tn", "TskReminder").Run()
+	// Delete scheduled task
+	err := exec.Command("schtasks", "/delete", "/tn", "TskReminder", "/f").Run()
 	if err != nil {
-		return fmt.Errorf("failed to end scheduled task: %w", err)
+		return fmt.Errorf("failed to delete scheduled task: %w", err)
 	}
 
 	return nil
